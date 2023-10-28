f7447665871303e3ec0f33b3f7c10e28ba53f0f5
wookki <bwlimtony@gmail.com>
2023-10-28 22:08:28 +0900

    feat: input 컴포넌트 제작

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -5,6 +5,7 @@ import LogoPath from "@/assets/colorful.svg";
 import { useRouter } from "next/navigation";
 import styles from "./Auth.module.scss";
 import Loader from "@/components/loader/Loader";
+import Input from "@/input/Input";
 const LoginClient = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -26,14 +27,35 @@ const LoginClient = () => {
 
   return (
     <>
-      <Loader />
+      {isLoading && <Loader />}
       <section className={styles.page}>
         <div className={styles.container}>
           <h1 className={styles.logo}>
             <Image src={LogoPath} alt="logo" />
           </h1>
           <form className={styles.form} onSubmit={loginUser}>
-            Input
+            <Input
+              email
+              icon="letter"
+              id="email"
+              name="email"
+              label="이메일"
+              placeholder="아이디(이메일)"
+              className={styles.control}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              password
+              icon="lock"
+              id="password"
+              name="password"
+              label="비밀번호"
+              placeholder="비밀번호"
+              className={styles.control}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <div className={styles.group}>자동 로그인, 비밀번호 수정</div>
             <div className={styles.buttonGroup}>
               Button
--- src/input/Input.jsx
+++ src/input/Input.jsx
@@ -1,5 +1,93 @@
-const Input = () => {
-  return <div></div>;
+import classNames from "classnames/bind";
+import styles from "./Input.module.scss";
+import Icon from "@/components/icon/Icon";
+import { useState } from "react";
+const cx = classNames.bind(styles);
+
+const Input = ({
+  id,
+  label,
+  name = "",
+  labelVisible,
+  icon,
+  email,
+  password,
+  placeholder = "",
+  readOnly,
+  disabled,
+  value,
+  error: errorProps,
+  className = "",
+  onChange,
+  ...restProps
+}) => {
+  const [inputValue, setInputValue] = useState(value ? value : "");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const checkType = () => {
+    if (email) {
+      return "email";
+    }
+
+    if (password) {
+      return isPasswordVisible ? "text" : "password";
+    }
+
+    return "text";
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+    onChange(e);
+  };
+
+  const iconType = isPasswordVisible ? "show" : "hide";
+  const iconLabel = `비밀번호 ${isPasswordVisible ? "표시" : "감춤"}`;
+
+  return (
+    <div className={cx(styles.formControl, className)}>
+      <label
+        htmlFor={id}
+        className={cx(styles.label, labelVisible || styles.labelHidden)}
+      >
+        {label}
+      </label>
+      <div
+        className={cx(
+          styles.inputWrapper,
+          errorProps && styles.inputWrapperError,
+        )}
+      >
+        {icon ? <Icon /> : null}
+        <input
+          id={id}
+          type={checkType()}
+          name={name}
+          className={cx(styles.input)}
+          placeholder={placeholder}
+          readOnly={readOnly}
+          disabled={disabled}
+          value={inputValue}
+          onChange={handleChange}
+          {...restProps}
+        />
+        {password ? (
+          <button
+            type="button"
+            className={cx(styles.button)}
+            onClick={() => setIsPasswordVisible((prev) => !prev)}
+            disabled={disabled}
+          >
+            <Icon type={iconType} alt={iconLabel} title={iconLabel} />
+          </button>
+        ) : null}
+      </div>
+      {errorProps && (
+        <span role="alert" className={cx(styles.error)}>
+          {errorProps.message}
+        </span>
+      )}
+    </div>
+  );
 };
 
 export default Input;
