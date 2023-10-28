7833cbef443eb32cc8e000c24728391a280b13db
wookki <bwlimtony@gmail.com>
2023-10-28 15:56:14 +0900

    feat: loader 컴포넌트 제작

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import Image from "next/image";
 import LogoPath from "@/assets/colorful.svg";
 import { useRouter } from "next/navigation";
-import styles from "Auth.module.scss";
+import styles from "./Auth.module.scss";
+import Loader from "@/components/loader/Loader";
 const LoginClient = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -24,21 +25,24 @@ const LoginClient = () => {
   const signInWithGoogle = () => {};
 
   return (
-    <section className={styles.page}>
-      <div className={styles.container}>
-        <h1 className={styles.logo}>
-          <Image src={LogoPath} alt="logo" />
-        </h1>
-        <form className={styles.form} onSubmit={loginUser}>
-          Input
-          <div className={styles.group}>자동 로그인, 비밀번호 수정</div>
-          <div className={styles.buttonGroup}>
-            Button
-            <div>Button</div>
-          </div>
-        </form>
-      </div>
-    </section>
+    <>
+      <Loader />
+      <section className={styles.page}>
+        <div className={styles.container}>
+          <h1 className={styles.logo}>
+            <Image src={LogoPath} alt="logo" />
+          </h1>
+          <form className={styles.form} onSubmit={loginUser}>
+            Input
+            <div className={styles.group}>자동 로그인, 비밀번호 수정</div>
+            <div className={styles.buttonGroup}>
+              Button
+              <div>Button</div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,5 +1,33 @@
-const Loader = () => {
-  return <div></div>;
+import { RotatingLines } from "react-loader-spinner";
+import styles from "./Loader.module.scss";
+
+const Loader = ({ basic }) => {
+  if (basic) {
+    return (
+      <div className={styles.basicWrapper}>
+        <RotatingLines
+          strokeColor="gray"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="30"
+          visible
+        />
+      </div>
+    );
+  }
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.loader}>
+        <RotatingLines
+          strokeColor="gray"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="30"
+          visible
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
