fd3a2310db8a771d20a47cb691bcf6a95c3c1934
wookki <bwlimtony@gmail.com>
2023-10-30 16:34:14 +0900

    feat: Header 컴포넌트 제작

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -1,5 +1,90 @@
+import styles from "./Header.module.scss";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { auth } from "@/firebase/firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { usePathname } from "next/navigation";
+import { router } from "next/client";
+import { toast } from "react-toastify";
+import {InnerH}
 const Header = () => {
-  return <div></div>;
+  const pathname = usePathname();
+  const [displayName, setDisplayName] = useState("");
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        if (user.displayName === null) {
+          const u1 = user.email.substring(0, user.email.indexOf("@"));
+          const uName = u1.charAt(0).toUpperCase() + u1.slice(1);
+          setDisplayName(uName);
+        } else {
+          setDisplayName(user.displayName);
+        }
+      } else {
+        setDisplayName("");
+      }
+    });
+  }, []);
+  const logoutUser = async () => {
+    try {
+      await signOut(auth);
+      toast.success("로그아웃 되었습니다.");
+      router.push("/");
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+
+  if (
+    pathname === "/login" ||
+    pathname === "/register" ||
+    pathname === "/reset"
+  ) {
+    return null;
+  }
+
+  return (
+    <header>
+      <div className={styles.loginBar}>
+        <ul className={styles.list}>
+          {!isLoggedIn ? (
+            <li className={styles.item}>
+              <Link href={"/login"}>로그인</Link>
+            </li>
+          ) : (
+            <>
+              <li className={styles.item}>
+                <Link href={"/admin/dashboard"}>관리자</Link>
+              </li>
+
+              <li className={styles.item}>
+                <Link href={"/order-history"}>주문 목록</Link>
+              </li>
+              <li className={styles.item}>
+                <Link href={"/"} onClick={logoutUser}>
+                  로그아웃
+                </Link>
+              </li>
+
+              <li className={styles.item}>
+                <Link href={"/"}>제휴 마케팅</Link>
+              </li>
+
+              <li className={styles.item}>
+                <Link href={"/"}>쿠팡 플레이</Link>
+              </li>
+
+              <li className={styles.item}>
+                <Link href={"/"}>고객센터</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+      {pathname.startsWith("/admin") ? null : <InnerHeader />}
+    </header>
+  );
 };
 
 export default Header;
