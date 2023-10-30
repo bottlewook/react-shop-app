f7731301cd275d9b892b3ff29ad426ffd31a41f5
wookki <bwlimtony@gmail.com>
2023-10-31 00:07:13 +0900

    feat: products 데이터 redux에 저장하기

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,12 +1,35 @@
 "use client";
 import useFetchCollection from "@/hooks/useFetchCollection";
 import styles from "./Product.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import {
+  GET_PRICE_RANGE,
+  selectProducts,
+  STORE_PRODUCTS,
+} from "@/redux/slice/productSlice";
+import ProductFilter from "@/components/product/productFilter/ProductFilter";
+import Loader from "@/components/loader/Loader";
+import ProductList from "@/components/product/productList/Produ`ctList";
 const Product = () => {
-  // const { data, isLoading } = useFetchCollection("products");
+  const { data, isLoading } = useFetchCollection("products");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(STORE_PRODUCTS({ products: data }));
+    dispatch(GET_PRICE_RANGE({ products: data }));
+  }, [data, dispatch]);
+
+  const products = useSelector(selectProducts);
+
   return (
     <section className={styles.product}>
-      <aside className={styles.filter}></aside>
-      <div className={styles.content}></div>
+      <aside className={styles.filter}>
+        {isLoading ? null : <ProductFilter />}
+      </aside>
+      <div className={styles.content}>
+        {isLoading ? <Loader basic /> : <ProductList />}
+      </div>
     </section>
   );
 };
